1109ced26e3d5a7983d13b5103f351d06845f51d
Vitor Carra <vitor.carra@ingaia.com.br>
2020-05-27 12:51:11 -0300

    looking for books by category links

--- books_webscrapping.py
+++ books_webscrapping.py
@@ -23,11 +23,21 @@ class BookStore:
 
         return books_store_page
 
-    def _gen_books(self, page: int):
-        catalog_page = f'{self.base_url}/catalogue/page-'
-        url_books_page = catalog_page + str(page) + '.html'
+    def _get_page(self, url:str):
+        page_source = get(url=url)
+        return bs(page_source.text, 'html.parser')
+        
+    def _gen_books(self, books_urls, category):
+        book = namedtuple('Book', 'name category price rating available')
 
-        books_catalog = get(url_books_page)
+        for url in books_urls:
+            book_url = f'{self.base_url}url'
+            book_content = self._get_page(book_url)
+            name = ''
+            price = ''
+            rating = ''
+            available = 0
+            yield book(name, category, price, rating, available)
 
         """
             books_list = books_catalog.find('section').find_all('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
@@ -37,18 +47,44 @@ class BookStore:
             ### name, price, rating, availability
         """
 
-    def _gen_category(self, url):
-        page_index = get(url)
-        total_books = int(first_page.find('form', {'class': 'form-horizontal'})
-                                    .find('strong').text
-                         ) #  first form shows the total books
+    def _gen_books_category(self, url):
+        try:
+            page_index = self._get_page(url)
+            total_books = int(page_index.find('form', {'class': 'form-horizontal'})
+                                        .find('strong').text
+                            ) #  first form shows the total books
+            category_name = url.split('/')[6].split('_')[0] #  category name is at 6th position in the URL
+            
+            if total_books <= 20:
+                total_pages = 1
+            else:
+                total_pages = total_books // 20 # we need to divide by 20 (max books in a page)
+                if total_books % 20 > 0: #  we check if is there a last page with less then 20
+                    total_pages += 1
 
-        if total_books <= 20:
-            total_pages = 1
-        else:
-            total_pages = total_books // 20 # we need to divide by 20 (max books in a page)
-            if total_books % 20 > 0: #  we check if is there a last page with less then 20
-                total_pages += 1
+            books_links = []
+            for page_number in range(1,total_pages+1):
+                print("{} - page {}".format(category_name, str(page_number)))
+                if page_number > 1:
+                    page_url = url.replace('index', 'page-' + str(page_number))
+                else:
+                    page_url = url
+                print(page_url)
+                page_content = self._get_page(page_url)
+                product_boxes = page_content.find_all('article',{'class': 'product_pod'})
+            
+                for book_link in product_boxes:
+                    books_links.append(book_link.find('a').get('href').replace('../',''))
+
+                #_gen_books(books_links, category_name)
+                print('Category: {}'.format(category_name))
+                print('books_links: {}'.format(books_links))
+                    
+
+            return True
+        except Exception as e:
+            print("Error: {}".format(e))
+            return False
             
 
     def scrap(self):
@@ -58,11 +94,13 @@ class BookStore:
                          ) #  first form shows the total books
 
         print("Total books to process: {}".format(str(total_books)))
-        
-
-        books_categories = [self.base_url + link.get('href') for link in first_page.find('div', {'class' : 'side_categories'}).find('ul', {'class': 'nav nav-list'}).find_all('a')]
-        print(books_categories)
         #print("Items per page: {}".format(str(self.items_per_page)))
+
+        books_categories = [self.base_url + link.get('href') for link in first_page.find('div', {'class' : 'side_categories'}).find('ul', {'class': 'nav nav-list'}).find('ul').find_all('a')]
+        #print(books_categories)
+
+        for category in books_categories:
+            all_books = self._gen_books_category(category)
 
 
 if __name__ == '__main__':
